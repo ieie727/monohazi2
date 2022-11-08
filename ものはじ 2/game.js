29ef683dbbb3cfa9df9f60f32bb5ccb623118764
ieie727 <103009461+ieie727@users.noreply.github.com>
2022-11-08 14:24:39 +0900

    Update game.js

--- ものはじ 2/game.js
+++ ものはじ 2/game.js
@@ -7,7 +7,7 @@ const h =canvas.height;
 //結果となる引数を乱数で決める
 const resultMin =3;
 const resultMax =20;
-const result =createRandomNumber(resultMin, resultMax);
+const result =Math.floor(Math.random() * (resultMax - resultMin) +resultMin);
 let catCount =0;
 let tigerCount =0;
 let totalCount =0;
@@ -19,15 +19,17 @@ const animal1 =new Image();
 const animal2 =new Image();
 const animal3 =new Image();
 const animal =[animal1,animal2,animal3];
-const animalX =[Math.random() *w, Math.random() *w, Math.random() *w];
-const animalY =[Math.random() *h, Math.random() *h, Math.random() *h];
-const animalType =["","",""];
+const animalX =["", "", ""];
+const animalY =["", "", ""];
+const animalType =["", "", ""];
 for(let i=0; i<animal.length; i++){
     selectPositionAndType(i); //X座標、Y座標、動物の種類を決定
 }
 
 //X座標、Y座標、動物の種類を決定
 function selectPositionAndType(i){
+    animalX[i] =Math.random() *w;
+    animalY[i] =Math.random() *h;
     //当たり範囲(またはその近く)に動物が召喚されそうな場合、Y座標の値を調整
     if(200 <=animalX[i] && animalX[i] <=800 && 150<=animalY[i] && animalY[i]<=450){
         if(animalY[i]<500){
@@ -97,78 +99,92 @@ function draw(){
 
     //動物たちを一匹ずつ動かす
     for(let i=0; i<animal.length; i++){
-        //動物の描画・移動
-        ctx.drawImage(animal[i], animalX[i], animalY[i], 50, 50);
-        //以下の傾きやx,yの動きは基本的にはコピペ予定
-        let tilt = (h/2 - animalY[i]) / (w/2 - animalX[i]); //中心と動物の現在地をつなぐ直線の傾き
-        let x = Math.sqrt(1 / (1 + tilt * tilt)); //Xの移動方向
-        let y =tilt * x; //Yの移動方向
-        let speed =2; //★★ここの値を変更すると動物の移動速度が倍速になる★★
-        if(animalX[i] <w/2){
-            animalX[i] += x *speed;
-            animalY[i] += y *speed;
-        }else{
-            animalX[i] -= x *speed;
-            animalY[i] -= y *speed;
-        }
-
+        moveAnimal(i); //動物の描画・移動
         ctx.drawImage(kotatsu, kotatsuX, kotatsuY, kotatsuSizeX, kotatsuSizeY); //コタツの描画
         
         //動物が当たり範囲内に入った時の処理
         if(atariXmin <=animalX[i] && animalX[i] <=atariXmax && atariYmin<=animalY[i] && animalY[i]<=atariYmax){
-            //それぞれの動物たちのカウンターを増やす
-            if(animalType[i]=="cat"){
-                catCount++;
-            }
-            else if(animalType[i]=="tiger"){
-                tigerCount++;
-            }
-            totalCount =catCount + tigerCount;
+            updateCount(i); //それぞれの動物たちのカウンターを増やす
 
             //動物の合計が最初の答えより少なかったら動物を再生成して繰り返す
-            if(totalCount+3 <=result){
+            if(totalCount +animal.length <=result){
                 selectPositionAndType(i);
             }
 
             //答えの分が既に生成されている場合は、画面外の遠くに作る
             else if(totalCount <result){
-                animalX[i] =Number.MAX_SAFE_INTEGER;
-                animalY[i] =Number.MAX_SAFE_INTEGER;
+                createAnimalToFar(i);
             }
 
             //動物の合計が最初の答えに達したら、正解発表の処理をする
             else{
-                clearInterval(id);
-                let answer =window.prompt("猫は何匹いるでしょうか？\n※半角数字で回答しましょう。");
-                ctx.fillStyle = "#B8E2FC";
-                ctx.fillRect(0, 0, w, h);
-
-                //正解・不正解の判定
-                ctx.font = "30px 'ＭＳ ゴシック'";
-                ctx.fillStyle = "#333333";
-                if(answer==catCount){    
-                    ctx.fillText("正解！ 正解は"+catCount+"匹です。", 250, 100);
-                }else{
-                    ctx.fillText("残念！ 正解は"+catCount+"匹です", 250, 100);
-                }
-                //答えの表示
-                showResult(catCount, 250, 250, cat);
-                showResult(tigerCount, 250, 350, tiger);
+                showCorrectAnswer();
                 return;
             }
         }
     }
 }
 
+
+//動物を動かす
+function moveAnimal(i){
+    ctx.drawImage(animal[i], animalX[i], animalY[i], 50, 50);
+
+    //以下の傾きやx,yの動きは基本的にはコピペ予定
+    let tilt = (h/2 - animalY[i]) / (w/2 - animalX[i]); //中心と動物の現在地をつなぐ直線の傾き
+    let x = Math.sqrt(1 / (1 + tilt * tilt)); //Xの移動方向
+    let y =tilt * x; //Yの移動方向
+    let speed =2; //★★ここの値を変更すると動物の移動速度が倍速になる★★
+    if(animalX[i] <w/2){
+        animalX[i] += x *speed;
+        animalY[i] += y *speed;
+    }else{
+        animalX[i] -= x *speed;
+        animalY[i] -= y *speed;
+    }
+}
+
+//それぞれの動物たちのカウンターを増やす
+function updateCount(i){
+    if(animalType[i]=="cat"){
+        catCount++;
+    }
+    else if(animalType[i]=="tiger"){
+        tigerCount++;
+    }
+    totalCount =catCount + tigerCount;
+}
+
+//遠くに動物を召喚する
+function createAnimalToFar(i){
+    animalX[i] =Number.MAX_SAFE_INTEGER;
+    animalY[i] =Number.MAX_SAFE_INTEGER;
+}
+
+//正解発表
+function showCorrectAnswer(){
+    clearInterval(id); //intervalの繰り返しを止める
+    let answer =window.prompt("猫は何匹いるでしょうか？\n※半角数字で回答しましょう。");
+    ctx.fillStyle = "#B8E2FC";
+    ctx.fillRect(0, 0, w, h);
+
+    //正解・不正解の判定
+    ctx.font = "30px 'ＭＳ ゴシック'";
+    ctx.fillStyle = "#333333";
+    if(answer==catCount){    
+        ctx.fillText("正解！ 正解は"+catCount+"匹です。", 250, 100);
+    }else{
+        ctx.fillText("残念！ 正解は"+catCount+"匹です", 250, 100);
+    }
+    //答えの表示
+    showResultAnimal(catCount, 250, 250, cat);
+    showResultAnimal(tigerCount, 250, 350, tiger);
+}
+
 //結果表示用の関数
-function showResult(animalCount, startX, animalY, animalImage){
+function showResultAnimal(animalCount, startX, animalY, animalImage){
     for(let i=0; i<animalCount; i++){
         let animalX =startX +(50 *i);
         ctx.drawImage(animalImage,animalX,animalY,animalSizeX,animalSizeY)
     }
-}
-
-//minからmaxまでの整数の乱数を出す
-function createRandomNumber(min, max){
-    return Math.floor(Math.random() * (max - min) +min);
 }
