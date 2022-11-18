19c5e64e9e684031c7d595a12b2e1bad1ae11dd8
ieie727 <103009461+ieie727@users.noreply.github.com>
2022-11-18 10:32:28 +0900

    Update game.js

--- ものはじ 2/game.js
+++ ものはじ 2/game.js
@@ -1,53 +1,15 @@
-//キャンバスの描画部分
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const w = canvas.width;
 const h = canvas.height;
 
-//結果となる引数を乱数で決める
+//答えの設定やカウント
 const resultMin = 3;
-const resultMax = 20;
+const resultMax = 10;
 const result = Math.floor( Math.random() * ( resultMax - resultMin ) + resultMin );
+let animalCount = 0;
 let catCount = 0;
 let tigerCount = 0;
-let totalCount = 0;
-
-//初期設定(一次元配列までなので配列でゴリ押しする。)
-const animal1 = new Image();
-const animal2 = new Image();
-const animal3 = new Image();
-const animal = [animal1, animal2, animal3];
-const animalXsize = 50;
-const animalYsize = 50;
-const animalX = ["", "", ""];
-const animalY = ["", "", ""];
-const animalType = ["", "", ""];
-for(let i=0; i<animal.length; i++){
-    selectPositionAndType(i); //X座標、Y座標、動物の種類を決定
-}
-
-//X座標、Y座標、動物の種類を決定
-function selectPositionAndType(i){
-    animalX[i] = Math.random() *w;
-    animalY[i] = Math.random() *h;
-    //当たり範囲(またはその近く)に動物が召喚されそうな場合、Y座標の値を調整
-    if( 200 <= animalX[i]  &&  animalX[i] <= 800  &&  150 <= animalY[i]  &&  animalY[i] <= 450 ){
-        if( animalY[i] < 500 ){
-            animalY[i] = 0;
-        }else{
-            animalY[i] = h;
-        }
-    }
-    //動物の種類（猫or虎）を乱数で決める
-    animalRandom = Math.floor( Math.random() * 2 );
-    if(animalRandom === 0){
-        animal[i].src = "image/cat.png";
-        animalType[i] = "cat";
-    }else{
-        animal[i].src = "image/tiger.png";
-        animalType[i] = "tiger";
-    }
-}
 
 //当たり判定の範囲の設定
 const atariXmin = 450;
@@ -65,13 +27,28 @@ const kotatsuYsize = 200;
 const kotatsuX = w/2 - kotatsuXsize/2;
 const kotatsuY = h/2 - kotatsuYsize/2;
 
-//猫・虎の最後に答えで使う用
+//動物の画像の読み込みや設定
+const animal0 = new Image();
+const animal1 = new Image();
+const animal2 = new Image();
+const animal = [animal0, animal1, animal2];
+let animalType = ["", "", ""];
+let animalX = [0, 0, 0];
+let animalY = [0, 0, 0];
+for(let i=0; i<animal.length; i++){
+    selectPositionAndType(i);
+}
+const animalXsize = 50;
+const animalYsize = 50;
+
+//答え表示のために猫と虎を用意
 const cat = new Image();
 cat.src = "image/cat.png";
 const tiger = new Image();
 tiger.src = "image/tiger.png";
 
-//スタート・リスタートボタンの設定（コピペのみ）
+
+//スタート・リスタートボタンの設定
 const startButton = document.getElementById('start');
 kotatsu.onload = () => ctx.drawImage(kotatsu, kotatsuX, kotatsuY, kotatsuXsize, kotatsuYsize);
 let id;
@@ -87,51 +64,81 @@ startButton.addEventListener('click', () => {
     }
 });
 
-
 function draw(){
     //初期描画
     ctx.fillStyle = "#B8E2FC";
     ctx.fillRect(0, 0, w, h);
-    
-    //あたり範囲
+
+    //当たり範囲の描画
     ctx.fillStyle = "#ffff";
     ctx.fillRect(atariXmin, atariYmin, atariXsize, atariYsize);
 
-    //動物たちを一匹ずつ動かす
+    //動物を一匹ずつ動かす
     for(let i=0; i<animal.length; i++){
-        moveAnimal(i); //動物の描画・移動
-        ctx.drawImage(kotatsu, kotatsuX, kotatsuY, kotatsuXsize, kotatsuYsize); //コタツの描画
+        //動物の描画・移動
+        moveAnimal(i);
+
+        //コタツの描画
+        ctx.drawImage(kotatsu, kotatsuX, kotatsuY, kotatsuXsize, kotatsuYsize);
         
-        //動物が当たり範囲内に入った時の処理
+        //当たり範囲に入った時の処理
         if( atariXmin <= animalX[i]  &&  animalX[i] <= atariXmax  &&  atariYmin <= animalY[i]  &&  animalY[i] <= atariYmax ){
-            updateCount(i); //それぞれの動物たちのカウンターを増やす
+            countAnimal(i);
 
-            //動物の合計が最初の答えより少なかったら動物を再生成して繰り返す
-            if(totalCount + animal.length <= result){
-                selectPositionAndType(i);
+            //コタツに入った匹数 + 画面に出てくる動物の数が設定した答え以下の場合
+            if(animalCount + animal.length <= result){
+                selectPositionAndType(i); //動物の位置を初期化する
             }
 
-            //答えの分が既に生成されている場合は、画面外の遠くに作る
-            else if(totalCount < result){
-                createAnimalToFar(i);
+            //答えの匹数分だけ既に生成済みの場合
+            else if(animalCount < result){
+                createAnimalToFar(i); //画面外の遠くに生成する
             }
 
-            //動物の合計が最初の答えに達したら、正解発表の処理をする
+            //こたつに入った動物の合計が、答えの数に達したら正解発表の処理をする。
             else{
                 showCorrectAnswer();
-                clearInterval(id); //intervalの繰り返しを止める
+                clearInterval(id);
                 return;
             }
         }
     }
 }
 
+//動物の初期位置や種類を決める
+function selectPositionAndType(i){
+    animalX[i] = Math.random() * w;
+    animalY[i] = Math.random() * h;
+    //召喚NGエリアの設定
+    const ngAreaXsize = w/10 * 2; //wの２割(200)
+    const ngAreaYsize = h/10 * 2; //hの２割(120)
+    const ngAreaXmin = atariXmin - ngAreaXsize;
+    const ngAreaXmax = atariXmax + ngAreaXsize;
+    const ngAreaYmin = atariYmin - ngAreaYsize;
+    const ngAreaYmax = atariYmax + ngAreaYsize;
+    if( ngAreaXmin <= animalX[i]  &&  animalX[i] <= ngAreaXmax && ngAreaYmin <= animalY[i]  &&  animalY[i] <= ngAreaYmax ){
+        if(animalY[i] <= h/2){
+            animalY[i] = 0;
+        }else{
+            animalY[i] = h;
+        }
+    }
 
-//動物を動かす
+    //動物の種類（猫or虎）を乱数で決める
+    const animalRandomNum = Math.floor( Math.random() * 2 );
+    if(animalRandomNum === 0){
+        animal[i].src = "image/cat.png";
+        animalType[i] = "cat";
+    }else{
+        animal[i].src = "image/tiger.png";
+        animalType[i] = "tiger";
+    }
+}
+
+//動物の描画・移動
 function moveAnimal(i){
     ctx.drawImage(animal[i], animalX[i], animalY[i], animalXsize, animalYsize);
 
-    //以下の傾きやx,yの動きは基本的にはコピペ予定
     let tilt = (h/2 - animalY[i]) / (w/2 - animalX[i]); //中心と動物の現在地をつなぐ直線の傾き
     let x = Math.sqrt(1 / (1 + tilt * tilt)); //Xの移動方向
     let y = tilt * x; //Yの移動方向
@@ -145,48 +152,47 @@ function moveAnimal(i){
     }
 }
 
-//それぞれの動物たちのカウンターを増やす
-function updateCount(i){
+//当たり範囲に入った動物をカウントする
+function countAnimal(i){
+    animalCount++;
     if(animalType[i] === "cat"){
         catCount++;
-    }
-    else if(animalType[i] === "tiger"){
+    }else if(animalType[i] === "tiger"){
         tigerCount++;
     }
-    totalCount = catCount + tigerCount;
 }
 
-//遠くに動物を召喚する
+//画面外に動物を生成する
 function createAnimalToFar(i){
     animalX[i] = Number.MAX_SAFE_INTEGER;
     animalY[i] = Number.MAX_SAFE_INTEGER;
 }
 
-//正解発表
+//答え合わせの処理
 function showCorrectAnswer(){
     //背景を青で塗り直す（リセットする）
     ctx.fillStyle = "#B8E2FC";
     ctx.fillRect(0, 0, w, h);
 
-    let answer = window.prompt("猫は何匹いるでしょうか？\n※半角数字で回答しましょう。");
-
     //正解・不正解の判定
+    const answer = window.prompt("猫は何匹いるでしょうか？\n※半角数字で回答しましょう。");
     ctx.font = "30px 'ＭＳ ゴシック'";
     ctx.fillStyle = "#333333";
     if(answer == catCount){    
-        ctx.fillText("正解！ 正解は"+catCount+"匹です。", 250, 100);
+        ctx.fillText("正解！ 正解は" + catCount + "匹です。", 250, 100);
     }else{
-        ctx.fillText("残念！ 正解は"+catCount+"匹です", 250, 100);
+        ctx.fillText("残念！ 正解は" + catCount + "匹です", 250, 100);
     }
+
     //答えの表示
-    showResultAnimal(catCount, 250, 250, cat);
-    showResultAnimal(tigerCount, 250, 350, tiger);
+    showAnimal(cat, catCount, 250, 250);
+    showAnimal(tiger, tigerCount, 250, 350);
 }
 
-//結果表示用の関数
-function showResultAnimal(animalCount, startX, animalY, animalImage){
-    for(let i=0; i<animalCount; i++){
-        let animalX = startX + (50 * i);
+//答えの引数を表示
+function showAnimal(animalImage, count, startX, animalY){
+    for(let i=0; i<count; i++){
+        animalX = startX + (50 * i);
         ctx.drawImage(animalImage, animalX, animalY, animalXsize, animalYsize);
     }
 }
